6ee19f3b77890bf7b40243aab4c858df7c300c86
semih esenturk <semihesenturk@outlook.com>
2022-05-09 11:52:15 +0300

    First Commit
    First Commit

---
+++ src/Projections/Sozluk.Projections.FavoriteWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using Sozluk.Projections.FavoriteWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
